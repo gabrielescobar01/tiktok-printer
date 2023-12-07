097885bbdb14884cff9b69b39f575a194501eb25
Gabriel Escobar <claudioescobardg31@gmail.com>
2023-12-06 23:45:09 -0300

    first

---
+++ public/words.js
@@ -0,0 +1,43 @@
+/*
+* WARNING: Please don't delete:
+*    var WORDS = `
+* and
+*    `; in the end of line
+*
+* TIPS:
+*    The safest way to add a word is to insert in the middle of the list
+*/
+
+var WORDS = `
+animal|tiger
+animal|crocodile
+animal|spider
+animal|butterfly
+animal|turtle
+animal|cricket
+animal|dragonfly
+animal|elephant
+animal|squirrel
+animal|chameleon
+animal|giraffe
+animal|horse
+animal|octopus
+animal|shrimp
+fruit|apple
+fruit|avocado
+fruit|cherry
+fruit|chocolate
+fruit|coconut
+fruit|guava
+fruit|strawberry
+sport|swimming
+sport|football
+sport|basketball
+sport|badminton
+sport|baseball
+sport|bowling
+sport|karate
+sport|marathon
+sport|wrestling
+sport|surfing
+`;
